e983b273fea169a615e3bd923caab846d15656ce
ophelie turenne <turenneophelie@gmail.com>
2022-10-06 13:51:27 +0400

    added model and validations to movies, list and bookmarks. Added migrationion for all of them

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :movies
+  # resources :lists
+  # resources :bookmarks
+
+  root to: 'lists#index'
+  resources :lists do
+    resources :movies
+    resources :bookmarks, only: [:create]
+  end
+  resources :bookmarks, only: [:destroy]
+
 end
